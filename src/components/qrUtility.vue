6a92162479200f7fe0c26371ca30e7bc8515d638
lichengxiao <lichengxiao1995@126.com>
2023-09-24 22:50:12 +0800

    增加字符长度判断和字节数判断（utf-8）

--- src/components/qrUtility.vue
+++ src/components/qrUtility.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="container">
+    <div class= "container">
         <qrcode-vue :value="url" :size="size" level="H" 
         :style="{border:'20px solid white'}" @click="refresh"></qrcode-vue>
         <input type="text" v-model="url" placeholder="请输入内容" />
         <h5>刷新页面 或者 点击二维码，会自动获取你剪切板的内容</h5>
+    </div>
+    <div class = "charNum">
+      <label>输入内容有 {{ charCount }} 个字符</label>
+      <label>输入内容有 {{ byteLength }} 个字节(UTF-8编码)</label>
     </div>
   </template>
 
@@ -18,6 +22,8 @@ export default {
     return {
       url: "https://www.bing.com", // 这里可以是你从后台获取的链接
       size: 250, // 这里可以是你想要的大小
+      charCount:0,
+      byteLength:0
     };
   },
   mounted() {
@@ -30,6 +36,12 @@ export default {
       .readText()
       .then((text) => {
         this.url = text;
+        this.charCount = this.url.length;
+        this.byteLength = this.url.replace(/[\u0000-\u007f]/g,"a")
+        .replace(/[\u0080-\u07ff]/g,"aa")
+        .replace(/[\u0800-\uffff]/g,"aaa").length;
+        console.log(this.byteLength);
+
       })
       .catch((err) => {
         console.error("读取剪切板失败", err);
@@ -41,10 +53,17 @@ export default {
 </script>
 
 <style scoped>
+
 .container {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.charNum {
+  display:flex;
+  flex-direction: column;
+  align-items: flex-start;
 }
 
 input {
